3d75ad5db0d7d4cd5c026ab732cb33945bbeea53
Jordan <hello@jyodann.com>
2024-09-16 12:14:47 +0800

    Finished up game

--- Assets/Scipts/GridManager.cs
+++ Assets/Scipts/GridManager.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.MPE;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
--- Assets/Scipts/PlayerTile.cs
+++ Assets/Scipts/PlayerTile.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.SocialPlatforms;
 using static GridManager;
 
 public class PlayerTile : GameTile
